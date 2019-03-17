4d1a0a50eb56a56f3f74554b8fe29daa69a4dd46
himt-097 <minhthaohaduc@gmail.com>
2019-03-17 18:59:19 +0700

    Fix Some Bug

--- src/helper.cpp
+++ src/helper.cpp
@@ -1,6 +1,12 @@
+#if defined(_WIN32) || defined(WIN32)
+    #include <windows.h>
+#else
+    #include <sys/types.h>
+    #include <sys/stat.h>
+#endif
+
 #include "helper.h"
-#include <sys/types.h>
-#include <sys/stat.h>
+
 
 string Helper::StringToUpper(const string &str) {
     string new_string = str;
@@ -27,8 +33,34 @@ void Helper::ConvertLastNameToSpace(string &str) { // "ha-duc-minh-thao" >> "ha 
 }
 void Helper::MakeClassDir(const string &new_class_name) {
     #if defined(_WIN32) || defined(WIN32)
-        _mkdir(new_class_name.c_str());
+        CreateDirectory(new_class_name.c_str(), NULL);
     #else
         mkdir(new_class_name.c_str(), 0733);
     #endif
+}
+
+void Helper::NormalizeStudent(Student& student) {
+    UpperFirstCharOfLetter(student.first_name);
+    UpperFirstCharOfLetter(student.last_name);
+    UpperFirstCharOfLetter(student.gender);
+    FormatDay(student.dob);
+}
+
+void Helper::UpperFirstCharOfLetter(string &str) {
+    for (int i = 0; i < str.length(); ++i) {
+        
+        if ((str[i] >= 'a' && str[i] <= 'z') && (i == 0 || str[i - 1] == ' ')) {
+            str[i] = str[i] - 'a' + 'A';
+        }
+    }
+}
+
+void Helper::FormatDay(string &str) {
+    for (int i = 0; i < str.length(); ++i) {
+        if (str[i] >= '0' && str[i] <= '9') {
+            continue;
+        } else {
+            str[i] = '/';
+        }
+    }
 }
