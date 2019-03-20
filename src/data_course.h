2465415ccec4b64e740c0fd3866f3cb9f1872115
hmhieu <43925008+hmhieu18@users.noreply.github.com>
2019-03-21 00:24:39 +0700

    add struct course

---
+++ src/data_course.h
@@ -0,0 +1,16 @@
+#ifndef DATA_COURSE_H
+#define DATA_COURSE_H
+
+#include <string>
+
+using std::string;
+
+struct Course {
+	string ID;
+	string Lecturer;
+	string startDay;
+	string endDay;
+	string name;
+};
+
+#endif
