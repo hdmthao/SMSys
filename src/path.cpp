58cf726778ec8efc8fde14eddce4dce9d8ed7dbc
himt-097 <minhthaohaduc@gmail.com>
2019-03-21 18:59:09 +0700

    Add SomeThing

--- src/path.cpp
+++ src/path.cpp
@@ -1,7 +1,7 @@
 #include "path.h"
 
-const string Path::CLASS_LIST = "data/class/class_list.txt"; // Path to save list of existed class
+const string Path::CLASS_LIST = "data/class/classes_list.txt"; // Path to save list of existed class
 const string Path::ACCOUNT = "data/account/account.txt";    // Path to save account data
-const string Path::CLASS = "data/class/";
-const string Path::IMPORT_STUDENT = "data/import/student/";
-const string Path::ALL_STUDENT = "data/student/students.txt";
+const string Path::CLASS = "data/class/";                  // Path to Class folder
+const string Path::IMPORT_STUDENT = "data/import/student/"; // Path to folder save import file
+const string Path::ALL_STUDENT = "data/student/students_list.txt"; // Path to save all student
